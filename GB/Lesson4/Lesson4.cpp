48b470bfb7bc15af631008ea782cb503546d7452
George <ssalimovgorikk@gmail.com>
2021-10-05 22:38:42 +0300

    To resolve the task 4.1 and the task 4.2.1

---
+++ GB/Lesson4/Lesson4.cpp
@@ -0,0 +1,89 @@
+// TestProject.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <stdio.h>
+
+// TASK 4.1----------------------------------
+// dec to hex
+void DecToHex(uint32_t dwValue)
+{
+	if (!dwValue)
+		return ;
+
+	// Move down to end
+	DecToHex(dwValue >>= 1);
+	// Move up to beg, therefore print
+	printf("%d", dwValue & 1);
+}
+
+// Тask 1
+void task4_1()
+{
+	printf("\nTask 4.1\n");
+	// Тест 01110110
+	DecToHex(118);
+	printf("\n");
+}
+
+// TASK 4.2.1--------------------------------
+
+int ExpoVarOne(int a, int b)
+{
+	if (b == 0)
+		return 1;
+	b--;
+
+	return a * ExpoVarOne(a, b);
+}
+
+// Тask 4.2.1
+void task4_2_1()
+{
+	printf("\nTask 4.2.1\n");
+
+	int nExpo = ExpoVarOne(5, 5);
+	// 3125
+	printf("nExpo = %d\n", nExpo);
+
+	nExpo = ExpoVarOne(2, 16);
+	// 65536
+	printf("nExpo = %d", nExpo);
+}
+
+// TASK 4.2.2--------------------------------
+// Task 4.2.2
+void task4_2_2()
+{
+	printf("\nTask 4.2.2\n");
+
+}
+
+// TASK 4.3----------------------------------
+// Task 4.3
+void task4_3()
+{
+	printf("\nTask 4.3\n");
+}
+
+
+int main()
+{
+	setlocale(0, "");
+
+	// Тask 1
+	task4_1();
+
+	// Тask 4.2.1
+	task4_2_1();
+
+	// Task 4.2.2
+	task4_2_2();
+
+	// Task 4.3
+	task4_3();
+
+    return 0;
+}
+
