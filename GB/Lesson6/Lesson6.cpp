281dbdcc6bfb50e9931289c20860ba3de038ff96
George <ssalimovgorikk@gmail.com>
2021-10-07 23:11:20 +0300

    Resolved task 6.3

--- GB/Lesson6/Lesson6.cpp
+++ GB/Lesson6/Lesson6.cpp
@@ -1,9 +1,12 @@
 // TestProject.cpp: определяет точку входа для консольного приложения.
 //
 
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "stdafx.h"
 #include <iostream>
 #include <stdio.h>
+#include <math.h>
 
 void _fastcall PrintArray(int *pArr, size_t dwSize)
 {
@@ -24,7 +27,7 @@ void task6_1()
 {
 	printf("\nTask 6.1\n");
 	int nArr[]{ 1, 9, 2, 5, 7, 6, 4, 3, 8 };
-	int nLength = sizeof(nArr) / sizeof(nArr[0]);
+	size_t nLength = sizeof(nArr) / sizeof(nArr[0]);
 
 
 	PrintArray(nArr, nLength);
@@ -45,29 +48,42 @@ void task6_1()
 // ------------------------------------------
 // TASK 6.3----------------------------------
 // ------------------------------------------
-
 // Function: f(x) = sqrt(|x|) + 5*x**3
-float FuncTPK(float x)
+float FuncTPK(float fX)
 {
-	return x;
+	return sqrt(fabs(fX)) + 5 * pow(fX, 3);
 }
 
+#define LENGTH 11
 
 // Тask 6.3
 void task6_3()
 {
-	/*/
-	printf("\nTask 4.2.1\n");
+	printf("\nTask 6.3\n");
 
-	int nExpo = ExpoVarOne(5, 5);
-	// 3125
-	printf("nExpo = %d\n", nExpo);
+	float nArr[LENGTH]{0};
 
-	nExpo = ExpoVarOne(2, 16);
-	// 65536
-	printf("nExpo = %d", nExpo);
-	*/
+	for (size_t i = 0; i < LENGTH; i++)
+	{
+		printf("Enter number: ");
+		scanf_s("%f", &nArr[i]);
+	}
+
+	for (int i = LENGTH - 1; i >= 0; i--)
+	{
+		if (FuncTPK(nArr[i]) > 400.)
+			printf("Excess result for %.2f\n", nArr[i]);
+	}
 }
+/*Example
+Task 6.3
+Enter number: 80
+Enter number: 1
+Enter number: 50
+Excess result for 50,00
+Excess result for 80,00
+*/
+// ------------------------------------------
 
 int main()
 {
