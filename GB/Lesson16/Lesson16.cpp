579b7323cbab945c61cb4b34b1ef94a6fa604b07
George <ssalimovgorikk@gmail.com>
2021-11-02 22:04:58 +0300

    Resolve task 16.2

--- GB/Lesson16/Lesson16.cpp
+++ GB/Lesson16/Lesson16.cpp
@@ -17,11 +17,13 @@ void printMass(char *pucData, size_t dwSize)
 	printf("\n");
 }
 
+// This is to ignore first 32 of charters, that disvisible
 enum eTEXTformat {
 	eNoUsing,
 	eUsing
 };
 
+/// Cesar encode text
 void CesarEncode(
 	char   *pcMessage,
 	size_t dwSize,
@@ -32,11 +34,13 @@ void CesarEncode(
 	{
 		pcMessage[i] = pcMessage[i] + nKey;
 
+		// Offset to left or to rigth
 		if (format && pcMessage[i] < 0x20)
 			pcMessage[i] += 0x20 * (1 - 2*((uint32_t)nKey >> 31));
 	}
 }
 
+/// Cesar decode text
 void CesarDecode(
 	char   *pcMessage,
 	size_t dwSize,
@@ -50,14 +54,13 @@ void CesarDecode(
 		format);
 }
 
-
 //-----------------
 void task16_1()
 {
 	printf("Task 16.1\n");
 
-	//char *mass = new char[17];
-	char mass[17];
+	char *mass = new char[17];
+	//char mass[17];
 	int nKey = 3;
 
 	gets_s(mass, 17);
@@ -74,6 +77,7 @@ void task16_1()
 	printf("Open  text: %s\n", mass);
 
  	printf("\n");
+	free(mass);
 }
 //-----------------
 
@@ -81,10 +85,78 @@ void task16_1()
 //-----------------
 // Task 16.2
 //-----------------
+enum eModeMeshing {
+	eEncode,
+	eDecode
+};
+
+/*
+   \fn       void Meshing(char   *pcMessage,
+	                      size_t dwSize,
+	                      int    *nKey,
+	                      int    nColumn,
+	                      eModeMeshing mode = eEncode)
+	\brief   Meshing input the text using key
+	\param   pcMessage (IN/OUT) - source text
+	         dwSize    (IN)     - size text
+			 nKey      (IN)     - array value for meshing by columns
+			 mode      (IN)     - mode work this function (encode/decode)
+	\remark  dwSize must be a multiple of nColumn
+*/
+void Meshing(
+	char   *pcMessage,
+	size_t dwSize,
+	int    *nKey,
+	int    nColumn,
+	eModeMeshing mode = eEncode) {
+
+	char *tmp = new char[nColumn];
+
+	for (size_t j = 0; j < dwSize / nColumn; j++)
+	{
+		memcpy(tmp, &pcMessage[nColumn * j], nColumn);
+
+		for (size_t i = 0; i < nColumn; i++)
+		{
+			if (mode == eEncode)
+				pcMessage[nColumn * j + i      ] = tmp[nKey[i]];
+			else
+				pcMessage[nColumn * j + nKey[i]] = tmp[i];
+		}
+	}
+
+	free(tmp);
+}
+
+// Òask 16.2
 void task16_2()
 {
+	// Control example
+	// Open  text: 12345678
+	// Close text: 31427586
+    // Open  text: 12345678
+
+	int nColumn = 4;
 	printf("Task 16.2\n");
+
+	char aucMass[] = {
+		'1', '2', '3', '4',
+		'5', '6', '7', '8','\0' };
+
+	int *aucKey = new int[nColumn];
+	aucKey[0] = 2;
+	aucKey[1] = 0;
+	aucKey[2] = 3;
+	aucKey[3] = 1;
+
+	printf("Open  text: %s\n", aucMass);
+	Meshing(aucMass, sizeof (aucMass) - 1, aucKey, nColumn);
+	printf("Close text: %s\n", aucMass);
+	Meshing(aucMass, sizeof(aucMass) - 1, aucKey, nColumn, eDecode);
+	printf("Open  text: %s\n", aucMass);
+
 	printf("\n");
+	free(aucKey);
 }
 //-----------------
 
